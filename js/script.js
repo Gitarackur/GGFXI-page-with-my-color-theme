000d8e328571fe3005f60df6fee3e2a9fba57a81
Gitarackur---Peter Osah <Peterosah5@gmail.com>
2020-02-07 11:04:56 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+ // Counter 
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+
---
+++ js/script1.js
@@ -0,0 +1,11 @@
+// let forexdata= await fetch("https://crossorigin.me/https://www.freeforexapi.com/api/live");
+// //     let forexJs = await forexdata.json();
+// //     console.log(forexJs);
+// // }
+
+// .then(Frex=>{
+//     //     console.log(Frex);
+//     // })
+//     // .catch(error=>{
+//     //     console.log(error)
+//     // })
